e67e4b5579fab91514ba7c4f95686d860dfb7fdd
GustavoHenriqueDK <gustavo.dickmann123@gmail.com>
2020-08-24 19:55:42 -0300

    - Material components implemented; - Lowercase and uppercase test;

--- app/src/main/java/com/example/textcaseconversion/MainActivity.kt
+++ app/src/main/java/com/example/textcaseconversion/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.textcaseconversion
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        testMethod()
+    }
+
+    private fun testMethod() {
+        var counter = 1
+        buttonGenerate.setOnClickListener {
+            //Just for testing :p
+            if (counter == 2) {
+                textView.text = editTextText.text.toString().toUpperCase(Locale.ROOT)
+                counter--
+            } else {
+                textView.text = editTextText.text.toString().toLowerCase(Locale.ROOT)
+                counter++
+            }
+        }
     }
 }
