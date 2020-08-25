5c07dccfc72e2027f6f52c7d4582a3065b845921
GustavoHenriqueDK <gustavo.dickmann123@gmail.com>
2020-08-25 14:23:19 -0300

    - new layout added;

--- app/src/main/java/com/example/textcaseconversion/MainActivity.kt
+++ app/src/main/java/com/example/textcaseconversion/MainActivity.kt
@@ -1,12 +1,12 @@
 package com.example.textcaseconversion
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
-import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    //todo: o texto não pode ser grande a ponto do scroll view ocupar toda a tela!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -19,9 +19,13 @@ class MainActivity : AppCompatActivity() {
         supportActionBar!!.hide()
         window.setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN)
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
     }
 
     private fun testMethod() {
+        button.setOnClickListener {
+            textView.text = editTextText.text.toString()
+        }
     }
 }
